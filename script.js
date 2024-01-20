25b24353b45f339c099ffd115094a5da10e00923
Satyam Kumar Singh <153851883+SatyamKumarSingh07@users.noreply.github.com>
2024-01-21 02:51:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed=new Typed(".multiple-text",{
+    strings:["Frontend Developer","Graphic Designer","Data Analyst","Part Time Chef"],
+    typeSpeed:50,
+    backSpeed:50,
+    backdelay:100,
+    loop:true
+});
